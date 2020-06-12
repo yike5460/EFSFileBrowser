ce794925ca09d7d8b6f605cf1500c45b2bb8dddc
yike5460 <yike5460@163.com>
2020-06-12 17:09:52 +0800

    Update listFile.py

--- listFile.py
+++ listFile.py
@@ -10,10 +10,8 @@ def pathTree(path):
     fp = None
     d = {'name': os.path.basename(path)}
     if os.path.isdir(path):
-        #d['type'] = "directory"
         d['member'] = [pathTree(os.path.join(path,x)) for x in os.listdir(path)]
     else:
-        #d['type'] = "file"
         fp = open(path, 'rb')
         d['size'] = str(getSize(fp)) + ' bytes'
     return d
